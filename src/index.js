151ea755bba6ad21aacc6c2f8d757f74691a03ea
Lalu M. Riza Rizky <riza.kepler@gmail.com>
2020-07-23 14:03:42 +0800

    Updated comments

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import { databaseURI, databaseOptions } from '#config/database';
 
 (async function initializeServer() {
   try {
-    connection.on('open', () => console.log('Connected to the database.')); // Outputs success message if database connection executed successfully
-    await connect(databaseURI, databaseOptions); // Connects to a MongoDB database based on loaded URI and config
+    // Outputs success message if database connection executed successfully
+    connection.on('open', () => console.log('Connected to the database.'));
+    // Connects to a MongoDB database based on loaded URI and config
+    await connect(databaseURI, databaseOptions);
+    // Listens to the defined port within HTTP protocol configuration
     http.createServer(app).listen(port, () => {
       console.log(`Running on ${environment} environment.`);
       console.log(`Listening to port ${port}.`);
-    }); // Listens to the defined port within HTTP protocol configuration
+    });
   } catch (error) {
     console.error(error); // Console logs any occuring error
   }
